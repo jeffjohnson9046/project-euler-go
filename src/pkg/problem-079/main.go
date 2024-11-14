3c2d25b41f762c5f6c1ecfcf824671e63ba2f11c
Jeff Johnson <jeff.johnson@farohealth.com>
2024-11-13 21:11:49 -0800

    Remove dead code

--- src/pkg/problem-079/main.go
+++ src/pkg/problem-079/main.go
@@ -8,30 +8,6 @@ import (
 	"time"
 )
 
-// Read the provided file.
-// func readFile(path string) ([]string, error) {
-// 	file, err := os.Open(path)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		return nil, err
-// 	}
-// 	defer func() {
-// 		if err := file.Close(); err != nil {
-// 			log.Fatalf("Error reading file %s: %v", path, err)
-// 		}
-// 	}()
-
-// 	fileScanner := bufio.NewScanner(file)
-// 	fileScanner.Split(bufio.ScanLines)
-
-// 	var fileLines []string
-// 	for fileScanner.Scan() {
-// 		fileLines = append(fileLines, fileScanner.Text())
-// 	}
-
-// 	return fileLines, nil
-// }
-
 // Sort the graph of digits
 func topologicalSort(graph map[rune][]rune) string {
 	inDegree := make(map[rune]int)
