0c02ef11b56897cc225a6b5bc4f269f70e2a8d3d
Jeff Johnson <jeffjohnson@Jeffs-Mac-mini.local>
2024-11-22 08:16:17 -0800

    Find first empty cell

--- src/pkg/problem-096/types/Puzzle.go
+++ src/pkg/problem-096/types/Puzzle.go
@@ -20,6 +20,16 @@ func (p Puzzle) WithElementAt(row int, col int, value int) Puzzle {
 	return p
 }
 
+func (p Puzzle) GetFirstEmptyCell() int {
+	for i, v := range p.Grid {
+		if v == 0 {
+			return i
+		}
+	}
+
+	return -1
+}
+
 func (p Puzzle) ToString() string {
 	var sb strings.Builder
 	for r := 0; r < 9; r++ {
@@ -27,6 +37,7 @@ func (p Puzzle) ToString() string {
 			p.ElementAt(r, 0), p.ElementAt(r, 1), p.ElementAt(r, 2),
 			p.ElementAt(r, 3), p.ElementAt(r, 4), p.ElementAt(r, 5),
 			p.ElementAt(r, 6), p.ElementAt(r, 7), p.ElementAt(r, 8))
+
 		if r == 2 || r == 5 {
 			sb.WriteString("------+-------+------\n")
 		}
